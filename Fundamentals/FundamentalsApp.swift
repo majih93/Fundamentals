be40391401fbb926dae58e201e7f16680e3b6df1
James Ma <t90laser93@gmail.com>
2022-12-06 22:47:00 +0900

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by 마지혁 on 2022/12/06.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
